870a9f21017e51150425d784ff63c0cd6d7ecf37
cmrule <cmrule@wm.edu>
2023-06-08 16:55:21 -0400

    Fixed issue with editing books being duplicated

--- Program.cs
+++ Program.cs
@@ -35,13 +35,14 @@ class ReadingList{
     public static void AddBookToList(Book book, List<Book> list){
         BookList.Add(book);
         empty = false;
-        Console.WriteLine($"\n{book.Title} by {book.Author} has been added to your readinglist.\n");
     }
 
-    public static bool RemoveBook(Book book, List<Book> list){
+    public static void RemoveBook(Book book, List<Book> list){
         bool success = list.Remove(book);
         if (list.Count() < 1){ empty = true;}
-        return success;
+        if (success){
+            Console.WriteLine($"\n{book.Title} by {book.Author} has been removed from your readinglist.\n");
+        }
     }
 
 }
@@ -50,11 +51,11 @@ class ReadingList{
 class Menu{
     static string currentOption = ""; //user selection from menu
     private static bool optionSelected = false; //whether or not the user has selected an option
+    //whether or not the user is editing information about an entry
+    private static bool CurrentlyEditing = false; 
     
     private static void MenuAddBook(List<Book> list){
         Book BookToAdd = new Book();
-        bool successfulParse = false;
-        int progress;
 
         Console.WriteLine("Please enter the title: ");
         BookToAdd.Title = Console.ReadLine();
@@ -62,28 +63,45 @@ class Menu{
         Console.WriteLine("Please enter the author's name: ");
         BookToAdd.Author = Console.ReadLine();
 
-        do {
-            Console.WriteLine("Please enter your progress in the book: ");
-            successfulParse = Int32.TryParse(Console.ReadLine(), out progress);
-        } while (!successfulParse);
-        BookToAdd.Progress = progress;
+        BookToAdd.Progress = ObtainProgress();
         
         //ask user if this info is correct before adding
         ConfirmBookInfo(BookToAdd, list);
 
     }
 
+    private static int ObtainProgress(){
+        int progress;
+        bool successfulParse = false;
+        do {
+            Console.WriteLine("Please enter your progress in the book: ");
+            successfulParse = Int32.TryParse(Console.ReadLine(), out progress);
+        } while (!successfulParse);
+        return progress;
+    }
+
     private static void ConfirmBookInfo(Book book, List<Book> list){
         Console.WriteLine("\nIs the following information about this book correct?");
-        Console.WriteLine("============================================");
+        Console.WriteLine("............................................");
         Console.WriteLine($"{book}");
-        Console.WriteLine("============================================\n");
+        Console.WriteLine("............................................\n");
         if (HelperConfirmationPrompt()){ //information is correct
+
+            //we are confirming that the edited information is correct,
+            //therefore we do not want to add another new book - we want
+            //to edit a book in place -- do not add a new one
+            if(CurrentlyEditing){
+                Console.WriteLine($"\nYour reading list has been successfully updated.\n");
+                CurrentlyEditing = false; //exiting the editing phase
+                return;
+            }
             ReadingList.AddBookToList(book, list);
+            Console.WriteLine($"\n{book.Title} by {book.Author} has been added to your readinglist.\n");
         }
-        else{  //information was not correct - ask the user to input it again
-            Console.WriteLine("Sorry about that!\nPlease try to input the information\nabout that book again.");
-            MenuAddBook(list); //try again
+        else{  //information was not correct - send them to edit function to make changes
+            Console.WriteLine("\nSorry about that!\nPlease try to input the information about that book again.");
+            MenuEditBook(book, list); //the boolean shows that the user denied the information
+
         }
     }
 
@@ -107,36 +125,27 @@ class Menu{
     }
 
     private static void AddFirstBook(List<Book> list){
-        Console.WriteLine("Your readinglist is empty! \nWould you like to add a book?");
-        
-        if (HelperConfirmationPrompt()){ //add first book
-            MenuAddBook(list);}  
-
-        
-        else{ //do not add a book
-            //TODO:
-            //what to add here?
-        }
+        Console.WriteLine("Your readinglist is empty! \nPlease add your first book.\n");
+        MenuAddBook(list);
     }
 
     private static void MenuRemoveBook(List<Book> list){
         Console.WriteLine("Please enter the number of the book which you would like to remove:");
 
         //find the book and send to the ReadingList method
-        ReadingList.RemoveBook(list[FindTargetBook(list)], list);
+        ReadingList.RemoveBook(FindTargetBook(list), list);
     }
 
     /*
     Returns the index of the book which is being edited/removed
     */
-    private static int FindTargetBook(List<Book> list){
+    private static Book FindTargetBook(List<Book> list){
         bool successfulParse = false; //whether or not an int input was received
         int targetIndex; //index of book to be removed
         int attempts = 0; //the number of times they have tried to input a number
 
-        
         foreach (var book in list){
-            Console.WriteLine($"{list.IndexOf(book)+1} : {book.Title}");
+            Console.WriteLine($"{list.IndexOf(book)+1} : {book.Title} by {book.Author}");
             //prints: 1 : {title} // 2 : {title} ...
         }
 
@@ -148,7 +157,7 @@ class Menu{
             if (targetIndex > list.Count() || targetIndex < 1){ successfulParse = false; } //index out of range
             attempts++;
         } while (!successfulParse);
-        return targetIndex-1;
+        return list[targetIndex-1];
     }
 
     public static void ShowBooks(List<Book> list){
@@ -158,35 +167,81 @@ class Menu{
             return;
         }
 
+        //not the user's first book:
+        //some readability and formatting before printing the list:
+        Console.WriteLine("Here is your readinglist:\n");
+        Console.WriteLine("............................................");
+
+        //printing the list with a dividing line under each entry
         foreach(var book in list){
             Console.WriteLine($"{list.IndexOf(book)+1} : {book}"); //number of book : title \n author \n progress
-            Console.WriteLine("============================================\n");
+            Console.WriteLine("............................................");
 
         }
+
+        //print how many books are in the list
+        string titlesPlurality = "titles"; //to adjust for if there is only one book in the list
+        if (list.Count() == 1){ titlesPlurality = "title";}
+        Console.WriteLine($"\nYour readinglist contains {list.Count()} {titlesPlurality}.\n");
     }
 
-    private static void MenuEditBook(List<Book> list){
-        int targetIndex;
+    private static void MenuEditBook(Book book, List<Book> list){
         bool successfulParse = false;
         int attempts = 0;
         int optionSelected;
+        CurrentlyEditing = true;
 
-        Console.WriteLine("Please enter the number of the book which you would like to edit:");
-        targetIndex = FindTargetBook(list);
-        Console.WriteLine($"What would you like to edit from {list[targetIndex].Title}?");
+        //check if list is empty
+        if (list.Count() == 0){
+            AddFirstBook(list);
+            return;
+        }
+        
+        Console.WriteLine($"\nWhat would you like to edit from {book.Title} by {book.Author}?");
 
         //obtain user option of which to edit
-        Console.WriteLine("1 : Title\n2: Author\n3: Progress");
+        Console.WriteLine("1 : Title\n2 : Author\n3 : Progress\n");
         do{
             //prompt invalid input 
             if (attempts>0){ Console.WriteLine("Invalid input."); }
             successfulParse = Int32.TryParse(Console.ReadLine(), out optionSelected);
-            if (targetIndex > list.Count() || targetIndex < 1){ successfulParse = false; } //index out of range
+            if (optionSelected > 3 || optionSelected < 1){ successfulParse = false; } //index out of range
             attempts++;
         } while (!successfulParse);
 
-        
+        switch (optionSelected){
+            case 1: //title
+                MenuEditTitle(book, list);
+                break;
+            case 2: //author
+                MenuEditAuthor(book, list);
+                break;
+            case 3: //progress
+                MenuEditProgress(book, list);
+                break;
+        }
+
+        ConfirmBookInfo(book, list);
     }
+
+    private static void MenuEditTitle(Book book, List<Book> list){
+        Console.WriteLine("\nWhat would you like to change the title of this entry to?");
+        book.Title = Console.ReadLine();
+    }
+
+    private static void MenuEditAuthor(Book book, List<Book> list){
+        Console.WriteLine("\nWhat would you like to change the author of this entry to?");
+        book.Author = Console.ReadLine();
+
+    }
+
+    private static void MenuEditProgress(Book book, List<Book> list){
+        Console.WriteLine("\n");
+        book.Progress = ObtainProgress();
+
+    }
+
+
     private static void DisplayMenu(List<Book> list){
 
         do{
@@ -220,7 +275,8 @@ class Menu{
                 break;
 
             case "E": //edit a book
-                MenuEditBook(list);
+                Console.WriteLine("Please enter the number of the book which you would like to edit:");
+                MenuEditBook(FindTargetBook(list), list);
                 break;
 
             case "D": //delete a book
@@ -236,14 +292,18 @@ class Menu{
     }
 
     class Program{
+        private static bool run = true;
         static void Main(string[] args){
-            while (ReadingList.empty){
-                Menu.DisplayMenu(ReadingList.BookList);
+            while (run){
+                while (ReadingList.empty){
+                    Menu.DisplayMenu(ReadingList.BookList);
+                }
+                while (!ReadingList.empty){
+                    //Menu.ShowBooks(ReadingList.BookList);
+                    Menu.DisplayMenu(ReadingList.BookList);
+                }
             }
-            while (!ReadingList.empty){
-                //Menu.ShowBooks(ReadingList.BookList);
-                Menu.DisplayMenu(ReadingList.BookList);
-            }
+            
         }
     }
     
